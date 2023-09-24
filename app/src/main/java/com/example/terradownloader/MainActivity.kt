248a140012c77fb65ec65c029701864b147a974b
Ankit Pratap <ankitprothebro@gmail.com>
2023-09-24 20:33:03 +0530

    Set up dynamic colors for App

---
+++ app/src/main/java/com/example/terradownloader/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.terradownloader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+}
