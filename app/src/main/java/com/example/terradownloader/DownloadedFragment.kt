6be194c74618bd10e38593802efb05185c41e74c
Ankit <ankitprothebro@gmail.com>
2023-12-20 22:11:38 +0530

    Added to dovnload list also

--- app/src/main/java/com/example/terradownloader/DownloadedFragment.kt
+++ app/src/main/java/com/example/terradownloader/DownloadedFragment.kt
@@ -1,10 +1,16 @@
 package com.example.terradownloader
 
+import QueuedViewModel
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.terradownloader.Adapter.QueuedAdapter
 
 
 /**
@@ -15,7 +21,8 @@ import androidx.fragment.app.Fragment
 class DownloadedFragment : Fragment() {
 
 
-
+    private val viewModel: QueuedViewModel by activityViewModels()
+    private lateinit var mQueuedAdapter: QueuedAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +34,20 @@ class DownloadedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val recyclerView: RecyclerView =
+            view.findViewById(R.id.recyclerview_fragment_downloaded)
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
+        //val mList = viewModel.getDownloadItems()
 
+        mQueuedAdapter = QueuedAdapter() // Pass an empty list initially
+        recyclerView.adapter = mQueuedAdapter
+
+
+        viewModel.getQueuedListData().observe(viewLifecycleOwner) { downloadList ->
+            if (downloadList.size > 3)
+                mQueuedAdapter.updateAdapter(downloadList.subList(1, downloadList.size-1))
+
+            Log.d("DownloadList lifecule", "Size: ${downloadList.size}")
+        }
     }
 }
